c75cc40db395f2c4c08ba37601ee2b8d4379ebdf
Jonathan Laluces <jlalucescareer123@gmail.com>
2021-05-14 20:29:22 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const Distube = require('distube');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+const distube = new Distube(client, {
+    searchSongs: false,
+    emitNewSongOnly: true,
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (msg) => {
+    if (
+        msg.content === '-sheesh' ||
+        msg.content === 'sheesh' ||
+        msg.content.includes('sheesh')
+    ) {
+        distube.play(msg, 'https://www.youtube.com/watch?v=k3Z91U_kMLA');
+        setTimeout(() => {
+            distube.stop(msg);
+        }, 13000);
+    } else if (msg.content === 'halosheesh') {
+        distube.play(msg, 'https://www.youtube.com/watch?v=co1VrBdaRhA');
+        setTimeout(() => {
+            distube.stop(msg);
+        }, 31000);
+    } else if (msg.content === 'les go' || msg.content.includes('les')) {
+        distube.play(msg, 'https://www.youtube.com/watch?v=rg5V1MFk9iA');
+        setTimeout(() => {
+            distube.stop(msg);
+        }, 3000);
+    } else if (msg.content === 'ascension') {
+        distube.play(msg, 'https://www.youtube.com/watch?v=m4oZZhpMXP4');
+        setTimeout(() => {
+            distube.stop(msg);
+        }, 13000);
+    } else if (msg.content === 'yo pierre') {
+        distube.play(msg, 'https://www.youtube.com/watch?v=cedjVT1QI-A');
+        setTimeout(() => {
+            distube.stop(msg);
+        }, 180000);
+    }
+});
+
+client.login(process.env.TOKEN);
